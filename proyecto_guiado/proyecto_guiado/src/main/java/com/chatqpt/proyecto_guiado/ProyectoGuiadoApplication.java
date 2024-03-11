05c7875b6931382563c9607c8e06c67e918324e2
EL-Suescun <santiagogiraldosuescun@gmail.com>
2024-03-10 20:18:23 -0500

    Se crea el ProyectoGuiadoApplication

---
+++ proyecto_guiado/proyecto_guiado/src/main/java/com/chatqpt/proyecto_guiado/ProyectoGuiadoApplication.java
@@ -0,0 +1,13 @@
+package com.chatqpt.proyecto_guiado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoGuiadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoGuiadoApplication.class, args);
+	}
+
+}
---
+++ proyecto_guiado/proyecto_guiado/src/test/java/com/chatqpt/proyecto_guiado/ProyectoGuiadoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chatqpt.proyecto_guiado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoGuiadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
